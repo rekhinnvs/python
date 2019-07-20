b59d8b4c11f9b235a676b98e751186bd0a121b13
rekhin <rekhin.nvs@gmail.com>
2019-07-20 11:27:45 +0530

    modify gitignore file

--- pytests/test_one.py
+++ pytests/test_one.py
@@ -1,6 +1,22 @@
+import pytest
+import random
+
+#@pytest.mark.customtest
+def test_send_data():
+    pass
+
+def power(x):
+    value = x ** random.randint(1,8)
+    print(f"Random value : {value}")
+
 def fun(x):
     return x+1
 
 def test_answer():
-    assert fun(5) == 5
-    
+    assert fun(5) == 6
+
+def test_modulo():
+    value = power(5)
+    print(f"Value of x: {value}")
+    #assert value %2 == 0, "Value was odd, should be even "
+    pass
